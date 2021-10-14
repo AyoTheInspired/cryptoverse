5e795fd7d39ff8b1646c94510dff9472d108ef39
AyoTheInspired <ayoinspired@gmail.com>
2021-10-14 22:04:34 +0100

    Setup Crypto RTK

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,3 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+	"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+	"x-rapidapi-key": "e1dd4f0602msh7906cf9baee7fbcp1c1427jsn50e16f1bcace",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+
+const createRequest = (url) => ({
+	url,
+	headers: cryptoApiHeaders,
+});
+
+export const cryptoApi = createApi({
+	reducerPath: "cryptoApi",
+	baseQuery: fetchBaseQuery({ baseUrl }),
+	endpoints: (builder) => ({
+		getCryptos: builder.query({
+			query: () => createRequest("/exchanges"),
+		}),
+	}),
+});
+
 // var options = {
 // 	method: "GET",
 // 	url: "https://coinranking1.p.rapidapi.com/exchanges",
