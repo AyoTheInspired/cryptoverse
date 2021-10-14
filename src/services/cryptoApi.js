81af4f6236733a5b3fdc929348b0d49e384a839c
AyoTheInspired <ayoinspired@gmail.com>
2021-10-15 00:22:31 +0100

    Coins can now be filtered based on names

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -17,18 +17,9 @@ export const cryptoApi = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl }),
 	endpoints: (builder) => ({
 		getCryptos: builder.query({
-			query: () => createRequest("/coins"),
+			query: (count) => createRequest(`/coins?limit=${count}`),
 		}),
 	}),
 });
 
 export const { useGetCryptosQuery } = cryptoApi;
-
-// var options = {
-// 	method: "GET",
-// 	url: "https://coinranking1.p.rapidapi.com/exchanges",
-// 	headers: {
-// 		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
-// 		"x-rapidapi-key": "e1dd4f0602msh7906cf9baee7fbcp1c1427jsn50e16f1bcace",
-// 	},
-// };
