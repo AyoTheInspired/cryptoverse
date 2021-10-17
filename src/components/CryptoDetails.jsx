51412e1bc8b965d72d14b8a8f55629f94c853964
AyoTheInspired <ayoinspired@gmail.com>
2021-10-17 22:10:40 +0100

    Setting up crypto details page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -25,6 +25,8 @@ const CryptoDetails = () => {
 	const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
 	const cryptoDetails = data?.data?.coin;
 
+	console.log(cryptoDetails);
+
 	const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
 	const stats = [
 		{
@@ -32,37 +34,40 @@ const CryptoDetails = () => {
 			value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`,
 			icon: <DollarCircleOutlined />,
 		},
-		{ title: "Rank", value: cryptoDetails.rank, icon: <NumberOutlined /> },
+		{ title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
 		{
 			title: "24h Volume",
-			value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`,
+			value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`,
 			icon: <ThunderboltOutlined />,
 		},
 		{
 			title: "Market Cap",
-			value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`,
+			value: `$ ${
+				cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)
+			}`,
 			icon: <DollarCircleOutlined />,
 		},
 		{
 			title: "All-time-high(daily avg.)",
-			value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`,
+			value: `$ ${millify(cryptoDetails?.allTimeHigh.price)}`,
 			icon: <TrophyOutlined />,
 		},
 	];
+
 	const genericStats = [
 		{
 			title: "Number Of Markets",
-			value: cryptoDetails.numberOfMarkets,
+			value: cryptoDetails?.numberOfMarkets,
 			icon: <FundOutlined />,
 		},
 		{
 			title: "Number Of Exchanges",
-			value: cryptoDetails.numberOfExchanges,
+			value: cryptoDetails?.numberOfExchanges,
 			icon: <MoneyCollectOutlined />,
 		},
 		{
 			title: "Aprroved Supply",
-			value: cryptoDetails.approvedSupply ? (
+			value: cryptoDetails?.approvedSupply ? (
 				<CheckOutlined />
 			) : (
 				<StopOutlined />
@@ -71,17 +76,19 @@ const CryptoDetails = () => {
 		},
 		{
 			title: "Total Supply",
-			value: `$ ${millify(cryptoDetails.totalSupply)}`,
+			value: `$ ${millify(cryptoDetails?.totalSupply)}`,
 			icon: <ExclamationCircleOutlined />,
 		},
 		{
 			title: "Circulating Supply",
-			value: `$ ${millify(cryptoDetails.circulatingSupply)}`,
+			value: `$ ${millify(cryptoDetails?.circulatingSupply)}`,
 			icon: <ExclamationCircleOutlined />,
 		},
 	];
 
-	return (
+	return !cryptoDetails ? (
+		<p>Loading...</p>
+	) : (
 		<Col className="coin-detail-container">
 			<Col className="coin-heading-container">
 				<Title level={2} className="coin-name">
@@ -92,7 +99,6 @@ const CryptoDetails = () => {
 					cap, and supply.
 				</p>
 			</Col>
-
 			<Select
 				defaultValue="7d"
 				className="select-timeperiod"
@@ -102,6 +108,29 @@ const CryptoDetails = () => {
 					<Option key={date}>{date}</Option>
 				))}
 			</Select>
+			// Line Chart
+			<Col className="stats-containeer">
+				<Col className="coin-value-statistics">
+					<Col className="coin-value-statistics-heading">
+						<Title level={3} className="coin-details-heading">
+							{cryptoDetails.name} Value Statistics
+						</Title>
+
+						<p>An overview showing the stats of {cryptoDetails.name} </p>
+					</Col>
+
+					{stats.map(({ icon, title, value }) => (
+						<Col key={title} className="coin-stats">
+							<Col className="coin-stats-name">
+								<Text>{icon}</Text>
+								<Text>{title}</Text>
+							</Col>
+
+							<Text className="stats">{value}</Text>
+						</Col>
+					))}
+				</Col>
+			</Col>
 		</Col>
 	);
 };
